2433e11d4286bc4c6e749064af31ae09f4b71fff
Marcin Swornowski <swor71@gmail.com>
2018-02-20 14:40:30 +0100

    Not working rendering of list items

--- 15.6-Stopwatch/script.js
+++ 15.6-Stopwatch/script.js
@@ -17,42 +17,12 @@ class Stopwatch {
     print() {
         this.display.innerText = this.format(this.times);
     }
-    format(times) {
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }
-
-    start() {
-        if (!this.running) {
-            this.running = true;
-            this.watch = setInterval(() => this.step(), 10);
-        }
-    }
-    stop() {
-        this.running = false;
-        clearInterval(this.watch);
-    }
-    lap() {
-        resultsList.appendChild(document.createElement('li')).innerText = 'Lap ' + lapCounter + ' : ' + stopwatch.format(this.times);
-    }
-    clearLaps() {
-        resultsList.innerHTML = "";
-    }
-
+    
+    
     step() {
         if (!this.running) return;
         this.calculate();
         this.print();
-    }
-    calculate() {
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
     }
 }
 
@@ -85,3 +55,6 @@ lap.addEventListener('click', () => {lapCounter++; stopwatch.lap(lapCounter)});
 
 const clearLaps = document.getElementById('clearLaps');
 clearLaps.addEventListener('click', () => stopwatch.clearLaps());
+
+const app = document.getElementById('app');
+ReactDOM.render(<Stopwatch />, app);
